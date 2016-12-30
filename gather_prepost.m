e3aa4751c529bceb757ed4e22cba2501c79c9c46
malcolm <me@jgmalcolm.com>
2016-12-30 16:54:01 -0500

    clean up big

--- gather_prepost.m
+++ gather_prepost.m
@@ -37,135 +37,22 @@ end
 
 
 
+function [segment idx] = extract_state_segment(data, window, stimulation_time, stimulation_duration, offset, sampling_frequency)
 
+  start_idx = floor((stimulation_time-window-offset)*sampling_frequency);
+  end_idx   = start_idx + floor(window*sampling_frequency);
 
-function [state_segment idx] = extract_state_segment(data, window, stimulation_time, stimulation_duration, offset, sampling_frequency)
-
-  segment_start_index = floor((stimulation_time-window-offset)*sampling_frequency);
-  segment_end_index   = segment_start_index + floor(window*sampling_frequency);
-
-  idx = segment_start_index:segment_end_index;
-  state_segment       = data(:, idx);
+  idx = start_idx:end_idx;
+  segment = data(:, idx);
 
 end
 
-%%%%%%%%%%%%
-%
-%
-%
-%%%%%%%%%%%%
-function [effect_segment idx] = extract_effect_segment(data, window, offset,  stimulation_time, stimulation_duration, sampling_frequency)
+function [segment idx] = extract_effect_segment(data, window, offset,  stimulation_time, stimulation_duration, sampling_frequency)
 
-segment_start_index = floor((stimulation_time+stimulation_duration+offset)*sampling_frequency);
-segment_end_index   = segment_start_index + floor(window*sampling_frequency);
+  start_idx = floor((stimulation_time+stimulation_duration+offset)*sampling_frequency);
+  end_idx   = start_idx + floor(window*sampling_frequency);
 
-idx = segment_start_index:segment_end_index;
-effect_segment      = data(:, idx);
-
-end
-
-%%%%%%%%%%%%
-%
-%
-%
-%%%%%%%%%%%%
-function [biomarker_value] = calculate_biomarker(data, biomarker, freqs, ds)
-
-switch biomarker(1:3)
-
-    case 'psd'
-        [biomarker_value] = calculate_power_biomarker(data, biomarker, freqs, ds);
-    case 'coh'
-        [biomarker_value] = calculate_coherence_biomarker(data, biomarker, freqs, ds);
-    case 'lin'
-        [biomarker_value] = calculate_line_length_biomarker(data);
-end
-
-end
-
-%%%%%%%%%%%%
-%
-%
-%
-%%%%%%%%%%%%
-function biomarker_value = calculate_power_biomarker(data, biomarker, freqs, ds)
-
-params.Fs       = ds;
-params.tapers   = [3 4];
-params.fpass    = freqs;
-
-% data            = (data - repmat(mean(data,2),1, size(data,2)));% ./ repmat(std(data,[],2), 1, size(data,2)); % ???
-data = zscore(data,0,2);
-[S, ~] = mtspectrumc(data',params);
-
-
-if size(S,2) == 1
-    biomarker_value       = mean(S);
-else
-    biomarker_value       = mean(sum(S));
-end
-
-end
-
-
-function biomarker_value = calculate_line_length_biomarker(data)
-
-biomarker_value = mean(sum(abs(diff(data(5:8, :)'))));
-
-end
-
-
-function biomarker = calculate_xcorr_biomarker(data)
-% @(x,y) max(abs(xcorr(x,y))) / (norm(x,2)*norm(y,2))
-end
-
-
-%%%%%%%%%%%%
-%
-%
-%
-%%%%%%%%%%%%
-function biomarker = calculate_coherence_biomarker(data, biomarker, ds)
-
-switch biomarker
-
-    case 'coh_theta'
-        fpass = 4:.1:7;
-    case 'coh_gamma'
-        fpass = 32:.1:50;
-    case 'coh_alpha'
-        fpass = 8:.1:15;
-    case 'coh_beta'
-        fpass = 16:.1:31;
-    case 'coh_all'
-        fpass = 1:.1:50;
-
-end
-
-a   = 0;
-% a   = a + sum(mscohere(data(1,:)',data(2,:)', [], [],fpass,ds));
-% a   = a + sum(mscohere(data(1,:)',data(3,:)', [], [],fpass,ds));
-% a   = a + sum(mscohere(data(1,:)',data(4,:)', [], [],fpass,ds));
-% a   = a + sum(mscohere(data(2,:)',data(3,:)', [], [],fpass,ds));
-% a   = a + sum(mscohere(data(2,:)',data(4,:)', [], [],fpass,ds));
-% a   = a + sum(mscohere(data(3,:)',data(4,:)', [], [],fpass,ds));
-
-a   = a + sum(mscohere(data(5,:)',data(6,:), [], [],fpass,ds));
-a   = a + sum(mscohere(data(5,:)',data(7,:), [], [],fpass,ds));
-a   = a + sum(mscohere(data(5,:)',data(8,:), [], [],fpass,ds));
-a   = a + sum(mscohere(data(6,:)',data(7,:), [], [],fpass,ds));
-a   = a + sum(mscohere(data(6,:)',data(8,:), [], [],fpass,ds));
-a   = a + sum(mscohere(data(7,:)',data(8,:), [], [],fpass,ds));
-
-% a   = a + sum(mscohere(data(1,:)',data(8,:), [], [],fpass,ds));
-% a   = a + sum(mscohere(data(2,:)',data(7,:), [], [],fpass,ds));
-% a   = a + sum(mscohere(data(3,:)',data(6,:), [], [],fpass,ds));
-% a   = a + sum(mscohere(data(4,:)',data(5,:), [], [],fpass,ds));
-
-% d   = sum(mscohere(data(4,:)',data(5,:), [], [],fpass,ds));
-q   =a/6;
-
-
-biomarker = q';
+  idx = start_idx:end_idx;
+  segment = data(:, idx);
 
 end
